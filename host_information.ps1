5dce27f382e3c260eb8973edf4238ccbe5ad325c
Edson Nascimento <edsonleandro21@gmail.com>
2020-01-03 21:00:04 -0300

    Adicionado ao meu repositorio

---
+++ host_information.ps1
@@ -0,0 +1,10 @@
+Get-VMHost | ? { $_.Name -match "ascenty-bl02-*" } | Sort-Object -Property name | % {
+    $version = $_.Version
+    $build = $_.Build
+    $nome = $_.Name 
+    $ipaddress = $_ | Get-VMHostNetworkAdapter | ? { $_.Name -match "vmk0" } | % {
+        $IP = $_.IP 
+    }
+    Write-Host "$nome + $IP + $version + $build" 
+}
+Write-Host "$nome --> $IP"
